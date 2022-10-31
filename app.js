934db4fbd3477bb48251e3eb3e2324b61421eecd
Yasir Khan <yasirkhanone22@gmail.com>
2022-10-31 23:47:34 +0500

    app updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send({ 'slackUsername': "YasirKhan", 'backend': true, 'age': 22, 'bio': "learning backend development to produce better quality developers" })
+    res.send({ 'slackUsername': "Yasir Khan", 'backend': true, 'age': 22, 'bio': "learning backend development to produce better quality developers" })
 })
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
